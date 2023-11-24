8cb87ff7bbe21a06830a2ee8f4564fa9186da136
Chandan Sayed <hangzhou635@gmail.com>
2023-11-24 13:24:48 +0600

    Details route fixed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -11,7 +11,7 @@ const Card = ({ service }) => {
       <div className={`card-body p-4`}>
         <p className="badge badge-secondary border-transparent p-1 text-lg lg:text-xl font-bold h-auto">${cost.toFixed(2)}</p>
         <h2 className="card-title font-semibold text-xl">{title}</h2>
-        <Link to={`/service/${id}`} className={`inline-flex text-lg py-2 px-2 text-white bg-yellow-600 hover:bg-opacity-80 w-fit border rounded-xl`}>
+        <Link to={`/service-details/${id}`} className={`inline-flex text-lg py-2 px-2 text-white bg-yellow-600 hover:bg-opacity-80 w-fit border rounded-xl`}>
           View Details
         </Link>
       </div>
